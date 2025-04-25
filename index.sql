90ece968fc091b7f450226c827269c89636f89da
MarkJur-afk <jurgenmarkk@gmail.com>
2025-04-25 09:09:25 +0300

    Employes Create

--- index.sql
+++ index.sql
@@ -1,9 +1,9 @@
+--loome kaks tabelit
+
 create database MarkIndex;
 
 use MarkIndex;
 
-
---loome kaks tabelit
 create table IndianCustomers
 (
 Id int identity(1,1),
@@ -17,6 +17,38 @@ Id int identity(1,1),
 Name nvarchar(25),
 Email nvarchar(25)
 )
+
+create table Employees
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(10),
+Salary nvarchar(50),
+DepartmentId int
+)
+
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (1, 'Tom', 'Male', 4000, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (2, 'Pam', 'Female', 3000, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (3, 'John', 'Male', 3500, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (4, 'Sam', 'Male', 4500, 2)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (5, 'Todd', 'Male', 2800, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (6, 'Ben', 'Male', 7000, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (7, 'Sara', 'Female', 4800, 3)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (8, 'Valarie', 'Female', 5500, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (9, 'James', 'Male', 6500, NULL)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (10, 'Russell', 'Male', 8800, NULL)
+
+select * from Employees
 
 --sisestame tabelisse andmeid
 insert into IndianCustomers (Name, Email)
@@ -59,7 +91,6 @@ exec spGetCustomers
 execute spGetCustomers
 
 select * from UKCustomers
-
 
 create proc spGetEmployeesByGenderAndDepartment
 @Gender nvarchar(20),
